ab4bd54b09b48f1a361d112017058e4998dbb9dd
Riad <riadk910@gmail.com>
2026-05-17 18:50:14 +0600

    upgrade next.js

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
         hostname: "**",
       },
     ],
